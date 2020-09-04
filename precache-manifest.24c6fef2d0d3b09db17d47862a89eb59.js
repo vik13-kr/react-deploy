3ac6ffd2e8056812ab4c75038d02de4f3d9a933e
vik13-kr <50275114+vik13-kr@users.noreply.github.com>
2020-09-04 09:33:39 +0530

    Updates

---
+++ precache-manifest.24c6fef2d0d3b09db17d47862a89eb59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e9c0968666bca37ed13653e4f24c86",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "98ab6e2d48172f795433",
+    "url": "/react-deploy/static/css/main.ec457e37.chunk.css"
+  },
+  {
+    "revision": "bb020a9d9753a202a418",
+    "url": "/react-deploy/static/js/2.1f52bb63.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-deploy/static/js/2.1f52bb63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98ab6e2d48172f795433",
+    "url": "/react-deploy/static/js/main.0b8d473b.chunk.js"
+  },
+  {
+    "revision": "60bd6e2071997bfa4f92",
+    "url": "/react-deploy/static/js/runtime-main.b3851881.js"
+  }
+]);
